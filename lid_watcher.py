46932c3d7abf431e06c0984ca9f830e5049ff9e5
Vyacheslav Chigrin <vchigrin@yandex-team.ru>
2024-03-27 17:10:20 +0300

    Add script for LID state watch on Sway.

---
+++ lid_watcher.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+import re
+import json
+import subprocess
+import time
+
+BUILTIN_DISPLAY_DEV = 'eDP-1'
+RE_MAIN_DISPLAY_DEV = re.compile('^(DP-[0-9]+)')
+MAIN_DISPLAY_PREFERRED_RESOLUTION = '1920x1080'
+g_last_seen_state = None
+
+def get_current_state() -> str:
+    with open('/proc/acpi/button/lid/LID/state') as f:
+        for line in f:
+            parts = line.strip().split()
+            if parts[0] == 'state:':
+                return parts[1]
+    return None
+
+
+def change_builtin_display_state(desired_state: str):
+    subprocess.check_call([
+        'swaymsg', 'output', BUILTIN_DISPLAY_DEV, desired_state
+    ])
+
+def find_main_display_dev() -> str:
+    content = subprocess.check_output([
+        'swaymsg', '-t', 'get_outputs'
+    ])
+    items = json.loads(content)
+    for item in items:
+        match = RE_MAIN_DISPLAY_DEV.match(item['name'])
+        if match:
+            return match.group(1)
+    return None
+
+def fix_main_display_resolution():
+    main_display_dev = find_main_display_dev()
+    if not main_display_dev:
+        return
+    subprocess.check_call([
+        'swaymsg', 'output', main_display_dev, 'res',
+        MAIN_DISPLAY_PREFERRED_RESOLUTION
+    ])
+
+
+def check_iteration():
+    global g_last_seen_state
+    current_state = get_current_state()
+    if current_state == g_last_seen_state:
+        return
+    if current_state == 'open':
+        change_builtin_display_state('enable')
+    elif current_state == 'closed':
+        change_builtin_display_state('disable')
+        fix_main_display_resolution()
+    g_last_seen_state = current_state
+
+
+def main():
+    while True:
+        check_iteration()
+        time.sleep(1)
+
+if __name__ == '__main__':
+    main()
