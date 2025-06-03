cb8a83b9078f2f1f57be78b116557f1f8d5f5681
Vyacheslav Chigrin <vchigrin@yandex-team.ru>
2025-06-03 11:12:14 +0300

    Add script for making screenshot without uploading.

--- screenshot_wayland.sh
+++ screenshot_wayland.sh
@@ -6,11 +6,7 @@
 # 4) click a window or select a rectangle area on your display;
 # 5) paste from X11 selection (middle mouse button) to share the link.
 # This script assumes that your yandex-team/LD login equals to your local user name.
-exec 9> /tmp/jing-lock.$USER
 
-flock -n 9 || exit 0
 T=$(date +"/tmp/%Y-%m-%d_%H.%M.%S.png")
 grim -g "$(slurp)" "$T"
-curl -gSs --upload-file "${T}" "https://jing:jing@jingdav.yandex-team.ru/vchigrin/${T}"
-exec 9>&-
 echo "$T" | wl-copy
