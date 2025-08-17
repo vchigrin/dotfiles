fa01ecc0e51ce5ef5011ff8f31d18c6da9bec39c
Vyacheslav Chigrin <vyacheslav.chigrin@izba.dev>
2025-08-17 19:33:45 +0300

    Add nvim-tree plugin.

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,3 +1,5 @@
 -- Split lines with K button.
 vim.api.nvim_set_keymap('n', 'K', 'i<CR><Esc>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<Esc>', ':noh<return><Esc>', {noremap = true})
+-- nvim-tree plugin
+vim.api.nvim_set_keymap('n', '<F3>', ':NvimTreeToggle<return>', {noremap = true})
